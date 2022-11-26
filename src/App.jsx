8529ac0fc9f0a07cae21c846c4c1d250ab763c67
John Paul Narvasa <johnpaul.narvasa@sprintshipworldwide.com>
2022-11-26 22:58:31 +0800

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './components/nav/Nav'
+import Main from './components/main/Main';
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+    )
+};
+
+export default App
